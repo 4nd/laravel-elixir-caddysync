9ddae0161875674116946a8609725b15ddcd32c1
Andy Graham <andy@31interactive.com>
2016-06-07 12:42:29 +0100

    Added options

--- index.js
+++ index.js
@@ -6,20 +6,26 @@ var Elixir = require('laravel-elixir')
 var portFinder = require('portfinder');
 var Task = Elixir.Task;
 
-Elixir.extend('caddySync', function() {
+Elixir.extend('caddySync', function(options) {
 
-    portFinder.basePort =  50000;
+    var _options = _.extend({},{
+        caddypath: 'caddy',
+        basePort: 50000,
+        bindTo: '127.0.0.1',
+    },options);
 
-    var caddyCmd = 'caddy';
+    portFinder.basePort = _options.basePort;
+
+    var caddyCmd = _options.caddypath;
 
     // Browsersync will only run during `gulp watch`.
     if (gutils.env._.indexOf('watch') > -1) {
         portFinder.getPort(function(err, port){
             if(!err) {
                 gulp.src('').pipe(shell(caddyCmd + ' -port ' + port));
-                Elixir.mixins.browserSync({ proxy: '127.0.0.1:'+port});
+                Elixir.mixins.browserSync({ proxy: _options.bindTo+':'+port});
             } else {
-                Elixir.Log.heading('Failed to find free port above ' + portFinder.basePort)
+                Elixir.Log.heading('Failed to find free port above ' + _options.basePort)
             }
         })
     }
